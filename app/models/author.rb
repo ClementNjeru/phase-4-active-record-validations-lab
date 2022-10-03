d50da7cdc2ff6677a91968e574f7cdb56acdd328
ClementNjeru <njeruclement36@gmail.com>
2022-10-03 15:15:10 +0300

    commit tree c71e1b30c2d46b529bef6d840b1d28045d6585fc

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,8 @@
 class Author < ApplicationRecord
+    #is expected to validate that :name cannot be empty/falsy
+        validates :name, presence: true
+    #is expected to validate that :name is case-sensitively unique (FAILED - 1)
+        validates :name, uniqueness: true
+    # is expected to validate that the length of :phone_number is 10 (FAILED - 2)
+        validates :phone_number, numericality: true, length: { is: 10 }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ApplicationRecord
+    # is expected to validate that :title cannot be empty/falsy (FAILED - 1)
+    validates :title, presence: true
+    # is expected to validate that the length of :content is at least 250 (FAILED - 2)
+    validates :content, length:  { minimum: 250 }
+    # is expected to validate that the length of :summary is at most 250 (FAILED - 2)
+    validates :summary, length: { maximum: 250 }
+    # is expected to validate that :category is either ‹"Fiction"› or ‹"Non-Fiction"› (FAILED - 2)
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+    # is expected not to allow :title to be ‹"True Facts"› (FAILED - 2)
+    validates :title, inclusion: {in: ["You Won't Believe These True Facts"]}
 end
